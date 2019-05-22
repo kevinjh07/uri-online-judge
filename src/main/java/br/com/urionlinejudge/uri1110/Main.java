478d31b1dea6852c80c9f662849d3fe6781f4bce
kevin <kjbenedito@stefanini.com>
2019-05-22 15:41:47 -0300

    Melhoria de performance URI-1110.

--- src/main/java/br/com/urionlinejudge/uri1110/Main.java
+++ src/main/java/br/com/urionlinejudge/uri1110/Main.java
@@ -1,47 +1,45 @@
 package br.com.urionlinejudge.uri1110;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 /**
- * 
  * @author kevin URI Online Judge | 1110 Throwing Cards Away
- *
  */
 public class Main {
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-
-		while (sc.hasNext()) {
-			int valor = sc.nextInt();
-			if (valor == 0) {
-				break;
-			}
-			StringBuilder builder = new StringBuilder();
-			builder.delete(0, builder.length());
-			int[] vetor = new int[valor];
-			for (int i = 0; i < vetor.length; i++) {
-				vetor[i] = i + 1;
-			}
-			builder.append("Discarded cards: ");
-			for (int i = 0; i < vetor.length - 1; i++) {
-				builder.append(vetor[i]);
-				if (i < vetor.length - 2) {
-					builder.append(", ");
-				}
-				int proximo = i + 1;
-				int atual = vetor[proximo];
-				for (int j = proximo; j < vetor.length - 1; j++) {
-					vetor[j] = vetor[j + 1];
-				}
-				vetor[vetor.length - 1] = atual;
-			}
-			builder.append("\n")
-				.append("Remaining card: ")
-				.append(vetor[vetor.length - 1]);
-			System.out.println(builder.toString());
-		}
-		sc.close();
-	}
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        InputStreamReader stream = new InputStreamReader(System.in);
+        BufferedReader reader = new BufferedReader(stream);
+        StringBuilder builder = new StringBuilder();
+        String str;
+        while ((str = reader.readLine()) != null) {
+            int valor = Integer.parseInt(str);
+            if (valor == 0) {
+                break;
+            }
+            builder.delete(0, builder.length());
+            int[] vetor = new int[valor];
+            for (int i = 0; i < vetor.length; ++i) {
+                vetor[i] = i + 1;
+            }
+            builder.append("Discarded cards: ");
+            for (int i = 0; i < vetor.length - 1; ++i) {
+                builder.append(vetor[i]);
+                if (i < vetor.length - 2) {
+                    builder.append(", ");
+                }
+                int proximo = i + 1;
+                int atual = vetor[proximo];
+                for (int j = proximo; j < vetor.length - 1; ++j) {
+                    vetor[j] = vetor[j + 1];
+                }
+                vetor[vetor.length - 1] = atual;
+            }
+            builder.append("\n").append("Remaining card: ").append(vetor[vetor.length - 1]);
+            System.out.println(builder.toString());
+        }
+    }
 
 }
