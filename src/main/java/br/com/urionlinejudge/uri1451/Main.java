cb97172958f63f4fe26e991a0c8bec0b96a45c47
kevin <kjbenedito@stefanini.com>
2019-05-24 10:34:11 -0300

    Melhoria de performance URI-1451.

--- src/main/java/br/com/urionlinejudge/uri1451/Main.java
+++ src/main/java/br/com/urionlinejudge/uri1451/Main.java
@@ -1,6 +1,8 @@
 package br.com.urionlinejudge.uri1451;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 /**
  * 
@@ -9,35 +11,35 @@ import java.util.Scanner;
  */
 public class Main {
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		
-		while (sc.hasNext()) {
-			char[] texto = sc.next().toCharArray();
-			StringBuilder builder = new StringBuilder();
-			boolean beiju = false;
-			int indice = 0;
-			for (int i = 0; i < texto.length; i++) {
-				switch (texto[i]) {
-				case '[':
-					beiju = true;
-					indice = 0;
-					break;
-				case ']':
-					beiju = false;
-					break;
-				default:
-					if (beiju) {
-						builder.insert(indice++, texto[i]);
-					} else {
-						builder.append(texto[i]);
-					}
-					break;
-				}
-			}
-			System.out.println(builder.toString());
-		}
-		sc.close();
-	}
+    public static void main(String[] args) throws IOException {
+        InputStreamReader stream = new InputStreamReader(System.in);
+        BufferedReader reader = new BufferedReader(stream);
+        String str;
+        while ((str = reader.readLine()) != null) {
+            StringBuilder builder = new StringBuilder();
+            boolean beiju = false;
+            int indice = 0;
+            for (int i = 0; i < str.length(); ++i) {
+                char texto = str.charAt(i);
+                switch (texto) {
+                    case '[':
+                        beiju = true;
+                        indice = 0;
+                        break;
+                    case ']':
+                        beiju = false;
+                        break;
+                    default:
+                        if (beiju) {
+                            builder.insert(indice++, texto);
+                        } else {
+                            builder.append(texto);
+                        }
+                        break;
+                }
+            }
+            System.out.println(builder.toString());
+        }
+    }
 
 }
