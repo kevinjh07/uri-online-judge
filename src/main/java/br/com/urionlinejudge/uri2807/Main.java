8bdf025d5e03305c541e38053d130507c46cf07b
kevin <kjbenedito@stefanini.com>
2019-05-28 17:16:25 -0300

    Melhorias URI-2807.

--- src/main/java/br/com/urionlinejudge/uri2807/Main.java
+++ src/main/java/br/com/urionlinejudge/uri2807/Main.java
@@ -13,22 +13,21 @@ public class Main {
         InputStreamReader stream = new InputStreamReader(System.in);
         BufferedReader reader = new BufferedReader(stream);
         int value = Integer.parseInt(reader.readLine());
-        int[] array = new int[value];
-        array[0] = 1;
+        int[] array = new int[--value];
         int a = 1;
         int b = 1;
         int sum;
-        for (int i = 1; i < value; ++i) {
+        for (int i = 0; i < value; ++i) {
             array[i] = b;
             sum = a + b;
             a = b;
             b = sum;
         }
         StringBuilder builder = new StringBuilder();
-        for (int i = --value; i > 0; --i) {
+        for (int i = --value; i > -1; --i) {
             builder.append(array[i]).append(" ");
         }
-        builder.append(array[0]);
+        builder.append(1);
         System.out.println(builder.toString());
     }
 
