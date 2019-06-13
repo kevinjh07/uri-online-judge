e613eb423fad9e8f5e0ba4d16f430d219b8f93c7
kevin <kjbenedito@stefanini.com>
2019-06-13 10:42:28 -0300

    URI-1068 utilizando a classe Stack.

---
+++ src/main/java/br/com/urionlinejudge/uri1068/stack/Main.java
@@ -0,0 +1,41 @@
+package br.com.urionlinejudge.uri1068.stack;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+/**
+ *
+ * URI Online Judge | 1068 Parenthesis Balance I
+ *
+ */
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        InputStreamReader stream = new InputStreamReader(System.in);
+        BufferedReader reader = new BufferedReader(stream);
+        String str;
+        while ((str = reader.readLine()) != null) {
+            Stack<String> stack = new Stack<String>();
+            for (int i = 0; i < str.length(); i++) {
+                String current = String.valueOf(str.charAt(i));
+                if (current.equals("(")) {
+                    stack.push(current);
+                } else if (current.equals(")")) {
+                    if (stack.isEmpty()) {
+                        stack.push(current);
+                        break;
+                    }
+                    stack.pop();
+                }
+            }
+            if (stack.isEmpty()) {
+                System.out.println("correct");
+            } else {
+                System.out.println("incorrect");
+            }
+        }
+    }
+
+}
